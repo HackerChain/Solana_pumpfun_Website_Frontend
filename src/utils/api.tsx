3006a84d1ab68f599f439c021ecf863655d81146
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-12-17 10:31:04 -0600

    [fix] sync with backend

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { SERVER_PORT } from "../config";
 const api = axios.create({
   // baseURL: `https://solana-pumpfun-website.render.com/api`,
-  baseURL: "http://localhost:3000/v1/api",
+  baseURL: `http://localhost:${SERVER_PORT}/v1/api`,
   headers: {
     "Content-Type": "application/json",
   },
