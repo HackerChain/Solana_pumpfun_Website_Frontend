bba7a2256a470dc2031ca26dffe960d8b3c6ca92
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-12-16 22:04:44 -0600

    [feat] variable box on Assets Page & test data

--- src/page/Assets.tsx
+++ src/page/Assets.tsx
@@ -1,7 +1,30 @@
+import { VariableBox } from "../components/AssetsPage/VariableBox";
+
+const Variable = {
+  currentBalance: {
+    value: 19881.61,
+    percent: 11.54,
+    profit: 5644.72,
+  },
+  totalProfit: 11234.21,
+  realisedProfit: 2651.05,
+  unrealisedProfit: 2651.05,
+  totalInvested: 2312.23,
+  totalTickersInvested: 6431,
+  successTickers: {
+    value: 148,
+    percent: 11.54,
+  },
+  feePaid: 2.5,
+};
+
 export const Assets = () => {
   return (
-    <>
-      <div>this is assets page</div>
-    </>
+    <div className="flex flex-col w-full h-full px-[190px]">
+      <VariableBox {...Variable} />
+      <hr className="w-full border-bg_gray_light border-t my-12" />
+
+      <div>table</div>
+    </div>
   );
 };
